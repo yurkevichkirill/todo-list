3927af4079be3a0c7bfc47c785fda52fc838b7e2
Yurkevich Kirill <123456789+odin@users.noreply.github.com>
2025-07-14 13:02:25 +0300

    Add start initialization

---
+++ src/auto-gen.js
@@ -0,0 +1,46 @@
+export function generateExample(){
+    if(localStorage.getItem("isInitialized")){
+        return;
+    }
+
+    const initialization = [
+        {
+            "todoItems": [
+            {
+                "title": "run",
+                "description": "3 rings at medium tempo",
+                "dueDate": "2025-07-14",
+                "priority": "High",
+                "isDone": true,
+                "isFavourite": false
+            },
+            {
+                "title": "gym",
+                "description": "make high intensity full body training",
+                "dueDate": "2025-07-15",
+                "priority": "Medium",
+                "isDone": false,
+                "isFavourite": true
+            }
+            ],
+            "title": "sport"
+        },
+        {
+            "todoItems": [
+            {
+                "title": "english",
+                "description": "B1 level",
+                "dueDate": "2025-12-30",
+                "priority": "High",
+                "isDone": false,
+                "isFavourite": true
+            }
+            ],
+            "title": "study"
+        }
+    ]
+
+    localStorage.setItem("projects", JSON.stringify(initialization));
+
+    localStorage.setItem("isInitialized", "true");
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { createAllTasks } from "./all-tasks.js";
 import { createFavourTasks } from "./important.js";
 import { createTodayTasks } from "./today.js";
 import { createWeekTasks } from "./week.js";
+import { generateExample } from "./auto-gen.js";
+
+generateExample();
 
 createAllTasks();
 addProjectDOM();
