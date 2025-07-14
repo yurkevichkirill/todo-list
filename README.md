a3928013dba6bdec18f349344efac6fdf22d1e98
Yurkevich Kirill <123456789+odin@users.noreply.github.com>
2025-07-14 21:03:10 +0300

    write readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# webpack-template
+In this project I learn how difficult to write huge projects. But I also repeat a lot of old technologies and very happy that I finish# webpack-template
--- src/auto-gen.js
+++ src/auto-gen.js
@@ -1,46 +1,46 @@
-export function generateExample(){
-    if(localStorage.getItem("isInitialized")){
-        return;
-    }
+export function generateExample() {
+  if (localStorage.getItem("isInitialized")) {
+    return;
+  }
 
-    const initialization = [
+  const initialization = [
+    {
+      todoItems: [
         {
-            "todoItems": [
-            {
-                "title": "run",
-                "description": "3 rings at medium tempo",
-                "dueDate": "2025-07-14",
-                "priority": "Low",
-                "isDone": true,
-                "isFavourite": false
-            },
-            {
-                "title": "gym",
-                "description": "make high intensity full body training",
-                "dueDate": "2025-07-15",
-                "priority": "Medium",
-                "isDone": false,
-                "isFavourite": true
-            }
-            ],
-            "title": "sport"
+          title: "run",
+          description: "3 rings at medium tempo",
+          dueDate: "2025-07-14",
+          priority: "Low",
+          isDone: true,
+          isFavourite: false,
         },
         {
-            "todoItems": [
-            {
-                "title": "english",
-                "description": "B1 level",
-                "dueDate": "2025-12-30",
-                "priority": "High",
-                "isDone": false,
-                "isFavourite": true
-            }
-            ],
-            "title": "study"
-        }
-    ]
+          title: "gym",
+          description: "make high intensity full body training",
+          dueDate: "2025-07-15",
+          priority: "Medium",
+          isDone: false,
+          isFavourite: true,
+        },
+      ],
+      title: "sport",
+    },
+    {
+      todoItems: [
+        {
+          title: "english",
+          description: "B1 level",
+          dueDate: "2025-12-30",
+          priority: "High",
+          isDone: false,
+          isFavourite: true,
+        },
+      ],
+      title: "study",
+    },
+  ];
 
-    localStorage.setItem("projects", JSON.stringify(initialization));
+  localStorage.setItem("projects", JSON.stringify(initialization));
 
-    localStorage.setItem("isInitialized", "true");
+  localStorage.setItem("isInitialized", "true");
 }
